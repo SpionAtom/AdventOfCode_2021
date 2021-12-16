56d0c554373dc38a177723bf3628c7f9eabca30b
Spyon <tdpb@gmx.de>
2021-12-16 09:58:58 +0100

    Day 15

--- Day 15/main.js
+++ Day 15/main.js
@@ -11,7 +11,8 @@ var height = input.length, width = input[0].length
 var grid = create2DArray(height, width, null)
 for (var y = 0; y < height; y++) {
     for (var x = 0; x < width; x++) {
-        grid[y][x] = {y: y, x: x, risk: Number(input[y].charAt(x)), visited: false, dist: Number.MAX_VALUE, prev: null}
+        //grid[y][x] = {y: y, x: x, risk: Number(input[y].charAt(x)), visited: false, dist: Number.MAX_VALUE, prev: null}
+        grid[y][x] = Number(input[y].charAt(x))
     }
 }
 
@@ -26,7 +27,7 @@ for (var multiplyerY = 0; multiplyerY < multiplyer; multiplyerY++) {
             for (x = 0; x < width; x++) {
                 var multiplyRisk = (Number(input[y].charAt(x)) + multiplyerY + multiplyerX) % 9
                 if (multiplyRisk == 0) multiplyRisk = 9
-                grid[multiplyerY * height + y][multiplyerX * width + x] = {y: multiplyerY * height + y, x: multiplyerX * width + x, risk: multiplyRisk, visited: false, dist: Number.MAX_VALUE, prev: null}
+                grid[multiplyerY * height + y][multiplyerX * width + x] = multiplyRisk
             }
         }       
 
@@ -34,99 +35,42 @@ for (var multiplyerY = 0; multiplyerY < multiplyer; multiplyerY++) {
 }
 height *= multiplyer
 width *= multiplyer
-for (var y = 0; y < height; y++) {
-    var l = ""
-    for (var x = 0; x < width; x++) {
-        l += grid[y][x].risk
-    }
-    console.log(l)
+
+
+
+
+var costs = create2DArray(height, width, Number.MAX_VALUE)
+costs[0][0] = 0
+
+// fill top border
+for (var w = 1; w < width; w++) {
+    costs[0][w] = costs[0][w - 1] + grid[0][w]
+}
+// fill left border
+for (var h = 1; h < height; h++) {
+    costs[h][0] = costs[h - 1][0] + grid[h][0]
 }
 
-
-
-
-// init start
-grid[0][0].dist = 0
-grid[0][0].prev = grid[0][0]
-
-var current = smallestUnvisited(grid)
-var visited = 0
-while (current != null && visited < 99999999) {    
-    //console.log("%d current: %o", visited, current)
-    current.visited = true   
-    visited++
-    
-    // neighbour above
-    if (current.y > 0) {
-        if (!grid[current.y - 1][current.x].visited) {
-            if (current.dist + grid[current.y - 1][current.x].risk < grid[current.y - 1][current.x].dist) {
-                grid[current.y - 1][current.x].dist = current.dist + grid[current.y - 1][current.x].risk
-                grid[current.y - 1][current.x] = current
-            }
+// do the minimum cost path algorithm up to 9 times
+// in case there are steps going left or up again
+// https://www.youtube.com/watch?v=lBRtnuxg-gU
+for (var steps = 1; steps <= 9; steps++) {
+    for (var row = 0; row < height; row++) {
+        for (var col = 0; col < width; col++) {
+            var min = Math.min(
+                col > 0 ? costs[row][col - 1] : Number.MAX_VALUE,
+                row > 0 ? costs[row - 1][col] : Number.MAX_VALUE,
+                col < width - 1 ? costs[row][col + 1] : Number.MAX_VALUE,
+                row < height - 1 ? costs[row + 1][col] : Number.MAX_VALUE,
+                )
+            costs[row][col] = min + grid[row][col]
+            if (row == 0 && col == 0)
+            costs[row][col] = 0
         }
-    }
-    // neighbour below
-    if (current.y < height - 1) {
-        if (!grid[current.y + 1][current.x].visited) {
-            if (current.dist + grid[current.y + 1][current.x].risk < grid[current.y + 1][current.x].dist) {
-                grid[current.y + 1][current.x].dist = current.dist + grid[current.y + 1][current.x].risk
-                grid[current.y + 1][current.x].prev = current
-            }
-        }
-    }
-    // neighbour left
-    if (current.x > 0) {
-        if (!grid[current.y][current.x - 1].visited) {
-            if (current.dist + grid[current.y][current.x - 1].risk < grid[current.y][current.x - 1].dist) {
-                grid[current.y][current.x - 1].dist = current.dist + grid[current.y][current.x - 1].risk
-                grid[current.y][current.x - 1].prev = current
-            }
-        }
-    }
-    // neighbour right
-    if (current.x < width - 1 ) {
-        if (!grid[current.y][current.x + 1].visited) {
-            if (current.dist + grid[current.y][current.x + 1].risk < grid[current.y][current.x + 1].dist) {
-                grid[current.y][current.x + 1].dist = current.dist + grid[current.y][current.x + 1].risk
-                grid[current.y][current.x + 1].prev = current
-            }
-        }
-    }
-
-    current = smallestUnvisited(grid)
+    }    
 }
-
-
-var current = grid[height - 1][width - 1]
-var steps = 0
-var totalrisk = 0
-while (current != grid[0][0] && steps < 99999999) {
-    steps++
-    totalrisk += current.risk
-     //console.log("%d %o", steps, current)
-     current = current.prev
- }
-//console.log("%d %o", steps, current)
-console.log("Total risk: %d", totalrisk)
+//output2DGrid(costs)
 
 
 
-
-
-function smallestUnvisited(grid) {
-    var height = grid.length, width = grid[0].length
-    var su = null
-    var smallest = Number.MAX_VALUE
-
-    for (y = 0; y < height; y++) {
-        for (x = 0; x < width; x++) {
-            if (!grid[y][x].visited && grid[y][x].dist < smallest) {
-                smallest = grid[y][x].dist
-                su = grid[y][x]
-                //console.log("found new smallest %d %d", y, x)
-            }
-        }
-    }
-    
-    return su
-}
+console.log("Minimum costs: %d", costs[height - 1][width - 1])

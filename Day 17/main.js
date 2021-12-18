9f9838a215f5f2e662ca3d1acb596f3ec3e00077
Spyon <tdpb@gmx.de>
2021-12-18 23:27:00 +0100

    Day 17

--- Day 17/main.js
+++ Day 17/main.js
@@ -4,6 +4,8 @@
 // input
 var input = readTextFile("input.txt");
 
-var params = input[0].match(/(\d+)/g)
-var target = {x: params[0], y: params[1], x2: params[2], y2: params[3]}
-console.log("x=%d..%d  y=%d..%d", target.x, target.y, target.x2, target.y2)
+var params = input[0].match(/([-]*\d+)/g).map(function(item) {
+    return parseInt(item, 10);
+});
+var target = {x: params[0], x2: params[1], y: params[2], y2: params[3]}
+console.log("x = %d .. %d,  y = %d .. %d", target.x, target.y, target.x2, target.y2)
